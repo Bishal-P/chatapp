e32184464a90a0d8d076efcfe8ff0cccb1e0fea6
= <=>
2024-01-27 23:04:57 +0000

    28 Jan 2024, 04:34

--- lib/pages/ProfileScreen.dart
+++ lib/pages/ProfileScreen.dart
@@ -21,41 +21,45 @@ class _ProfileScreenState extends State<ProfileScreen> {
         title: Text("Profile", style: Theme.of(context).textTheme.headline4),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                // borderRadius: BorderRadius.circular(100)
-              ),
-              child: Stack(
-                children: [
-                  SizedBox(
-                    width: 120,
-                    height: 120,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child:
-                            const Image(image: AssetImage("assets/login.jpg"))),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      width: 35,
-                      height: 35,
-                      decoration: BoxDecoration(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  // borderRadius: BorderRadius.circular(100)
+                ),
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
                           borderRadius: BorderRadius.circular(100),
-                          color: Colors.black),
-                      child: const Icon(Icons.camera,
-                          color: Colors.black, size: 20),
+                          child: const Image(
+                              image: AssetImage("assets/login.jpg"))),
                     ),
-                  ),
-                ],
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Colors.black),
+                        child: const Icon(Icons.camera,
+                            color: Colors.black, size: 20),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
