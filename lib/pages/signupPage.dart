98716df511da9fa36db1d82884cca98859f31c0e
= <=>
2024-01-27 22:04:51 +0000

    28 Jan 2024, 03:34

--- lib/pages/signupPage.dart
+++ lib/pages/signupPage.dart
@@ -41,7 +41,11 @@ class _SignupPageState extends State<SignupPage> {
           uiHelper.CustomAlertBox(
               context, "Alert", "The password provided is too weak.");
         } else if (e.code == 'email-already-in-use') {
-          
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Email and Password cannot be empty"),
+            backgroundColor: Colors.redAccent,
+            elevation: 10, //shadow
+          ));
           // uiHelper.CustomAlertBox(
           //     context, "Info", "The account already exists for that email.");
         }
