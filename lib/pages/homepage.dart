a22a69695e97975f5074d58fb0414e8cbe88ca8a
= <=>
2024-01-27 21:37:44 +0000

    28 Jan 2024, 03:07

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -80,11 +80,13 @@ class _HomePageState extends State<HomePage> {
                       print(
                           "The snapshot data is ${snapshot.data?.docs[index].id}");
                       if (snapshot.data?.docs[index].id != api.user.uid) {
-                        
+                        return chartUsercard(
+                            name: snapshot.data?.docs[index]["name"],
+                            about: snapshot.data?.docs[index]["about"]);
                       }
-                      return chartUsercard(
-                          name: snapshot.data?.docs[index]["name"],
-                          about: snapshot.data?.docs[index]["about"]);
+                      // return chartUsercard(
+                      //     name: snapshot.data?.docs[index]["name"],
+                      //     about: snapshot.data?.docs[index]["about"]);
                     });
               } else {
                 return const Center(child: CircularProgressIndicator());
