013917e88037b3e4781fe01b8db3d5d34bbb4b5d
= <=>
2024-01-27 20:40:31 +0000

    28 Jan 2024, 02:10

--- lib/components/apis.dart
+++ lib/components/apis.dart
@@ -22,7 +22,7 @@ class api {
   static Future<void> createUser() async {
     final ChatUser chatUser = ChatUser(
       image: user.photoURL ?? "",
-      name: user.displayName ?? "NewUser",
+      name: user.displayName ?? "ABC",
       about: "Hey there! I am using ChatApp",
       createdAt: DateTime.now().toString(),
       lastActive: DateTime.now().toString(),
