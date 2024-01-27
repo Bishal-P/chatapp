f0b575f55e7584b2128c4bc03f4a4abcb43e3d87
= <=>
2024-01-27 21:49:09 +0000

    28 Jan 2024, 03:19

--- lib/pages/signupPage.dart
+++ lib/pages/signupPage.dart
@@ -17,6 +17,11 @@ class _SignupPageState extends State<SignupPage> {
 
   signUp() async {
     if (passwordController.text != confirmPasswordController.text) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("Password and Confirm Password does not match"),
+        backgroundColor: Color.fromARGB(255, 255, 0, 0),
+        elevation: 10, //shadow
+      ));
       uiHelper.CustomAlertBox(
           context, "Error", "Password and Confirm Password does not match");
       return;
