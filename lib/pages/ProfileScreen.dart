ae41d91081bef4b312048ae544aab7107060b315
= <=>
2024-01-27 22:43:43 +0000

    28 Jan 2024, 04:13

--- lib/pages/ProfileScreen.dart
+++ lib/pages/ProfileScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
@@ -9,6 +10,17 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => ProfileScreen()));
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_rounded,
+                color: Colors.white)),
+        title: Text("Profile", style: Theme.of(context).textTheme.headline4),
+      ),
+    );
   }
 }
