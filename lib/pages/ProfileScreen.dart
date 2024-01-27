b3335cc02bfe94169d6682a21b86f0c0961a188f
= <=>
2024-01-27 23:03:33 +0000

    28 Jan 2024, 04:33

--- lib/pages/ProfileScreen.dart
+++ lib/pages/ProfileScreen.dart
@@ -30,6 +30,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
               child: Stack(
                 children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child:
+                            const Image(image: AssetImage("assets/login.jpg"))),
+                  ),
                   Positioned(
                     bottom: 0,
                     right: 0,
@@ -42,14 +50,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       child: const Icon(Icons.camera,
                           color: Colors.black, size: 20),
                     ),
-                  ),
-                  SizedBox(
-                    width: 120,
-                    height: 120,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child:
-                            const Image(image: AssetImage("assets/login.jpg"))),
                   ),
                 ],
               ),
