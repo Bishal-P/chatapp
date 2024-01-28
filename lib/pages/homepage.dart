47431a94adf298f2594d2b21024e5616068c0fcf
= <=>
2024-01-28 11:13:04 +0000

    28 Jan 2024, 16:43

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -88,7 +88,9 @@ class _HomePageState extends State<HomePage> {
                     itemBuilder: (context, index) {
                       // print(
                       // "The snapshot data is ${snapshot.data?.docs[index].id}");
-                      if (snapshot.data?.docs[index].id != api.user.uid) {
+                      if (snapshot.data?.docs[index].id == api.user.uid) {
+                        print("the value is true");
+                      } else {
                         print("The snapshot data is ${snapshot.data?.docs}");
                         return chartUsercard(
                             name: snapshot.data?.docs[index]["name"],
