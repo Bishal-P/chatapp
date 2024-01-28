a7bf2ab3888c7a4d2cac7684ca48c185398c288d
= <=>
2024-01-28 20:41:43 +0000

    29 Jan 2024, 02:11

--- lib/pages/ProfileScreen.dart
+++ lib/pages/ProfileScreen.dart
@@ -81,6 +81,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 child: Column(
                   children: [
                     TextFormField(
+                      initialValue: data?["name"],
                       decoration: const InputDecoration(
                           border: const OutlineInputBorder(
                               borderRadius:
