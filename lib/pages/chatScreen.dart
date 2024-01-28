e6297b936f9b029ce61dff05b505a7eec7b54a63
= <=>
2024-01-28 16:06:08 +0000

    28 Jan 2024, 21:36

--- lib/pages/chatScreen.dart
+++ lib/pages/chatScreen.dart
@@ -46,7 +46,7 @@ class _ChatScreenState extends State<ChatScreen> {
                         style: const TextStyle(
                           fontSize: 20,
                         )),
-                    Text(widget.doc!['status'].toString(),
+                    Text(widget.doc!['isOnline'].toString(),
                         style: const TextStyle(
                           fontSize: 10,
                         ))
